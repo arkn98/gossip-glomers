d1d292d25971e3387a6975ab0cce211c0af36420
arkn98 <20590666+arkn98@users.noreply.github.com>
2024-01-01 22:56:51 -0500

    Add Challenge #3b

--- 03-broadcast/3b-multi-node-broadcast/main.go
+++ 03-broadcast/3b-multi-node-broadcast/main.go
@@ -12,7 +12,8 @@ type server struct {
 	node *maelstrom.Node
 	mu   sync.RWMutex
 
-	seen []int
+	seen      map[int]bool
+	neighbors []string
 }
 
 func (s *server) HandleBroadcast(msg maelstrom.Message) error {
@@ -24,26 +25,57 @@ func (s *server) HandleBroadcast(msg maelstrom.Message) error {
 	toAdd := int(body["message"].(float64))
 
 	s.mu.Lock()
-	s.seen = append(s.seen, toAdd)
+	if _, ok := s.seen[toAdd]; ok {
+		s.mu.Unlock()
+		return nil
+	}
+	s.seen[toAdd] = true
 	s.mu.Unlock()
+
+	// broadcast this message to our neighbors
+	go s.Broadcast(body)
 
 	return s.node.Reply(msg, map[string]any{
 		"type": "broadcast_ok",
 	})
 }
 
+func (s *server) Broadcast(body any) {
+	for _, dest := range s.neighbors {
+		s.node.Send(dest, body)
+	}
+}
+
 func (s *server) HandleRead(msg maelstrom.Message) error {
 	s.mu.RLock()
 	defer s.mu.RUnlock()
 
+	keys := make([]int, 0, len(s.seen))
+	for k, _ := range s.seen {
+		keys = append(keys, k)
+	}
+
 	return s.node.Reply(msg, map[string]any{
 		"type":     "read_ok",
-		"messages": s.seen,
+		"messages": keys,
 	})
 }
 
+type TopologyReq struct {
+	Type     string              `json:"type"`
+	Topology map[string][]string `json:"topology"`
+}
+
 func (s *server) HandleTopology(msg maelstrom.Message) error {
-	// do nothing for now
+	var body TopologyReq
+	if err := json.Unmarshal(msg.Body, &body); err != nil {
+		return err
+	}
+
+	s.mu.Lock()
+	s.neighbors = body.Topology[s.node.ID()]
+	s.mu.Unlock()
+
 	return s.node.Reply(msg, map[string]any{
 		"type": "topology_ok",
 	})
@@ -51,8 +83,12 @@ func (s *server) HandleTopology(msg maelstrom.Message) error {
 
 func main() {
 	n := maelstrom.NewNode()
-	s := &server{node: n}
+	s := &server{
+		node: n,
+		seen: make(map[int]bool),
+	}
 
+	n.Handle("broadcast_ok", nil)
 	n.Handle("broadcast", s.HandleBroadcast)
 	n.Handle("read", s.HandleRead)
 	n.Handle("topology", s.HandleTopology)
